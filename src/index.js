43987870eb4300b9eb6f1c2cfd9df18400d14373
tandav21 <tvdavyd@gmail.com>
2021-03-15 20:33:50 +0300

    feat: add js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [];
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    let current = bracketsConfig[i];
+    if (current === '(' || current === '[' || current === "{") {
+      stack.push(current);
+  }
+  else if (current === ')' || current === ']' || current === "}") {
+    let lastBracket =  stack.pop();
+    
+    if (str[lastBracket] !== current) { 
+    
+      return false; 
+    }
+  }
+}
+return stack.length === 0;
 }
